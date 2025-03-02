7d5a4b42588283721591aa58dc2a19d980793f8a
Алексей Константинов <konstantinovalex123@outlook.com>
2025-03-02 13:49:35 +0300

    fix

--- Task1_7/Program.cs
+++ Task1_7/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             int a = Convert.ToInt32(Console.ReadLine());
-            if (a >= 10 && a < 100) Console.WriteLine("Да");
+            if (a > -100 && a < 100) Console.WriteLine("Да");
             else Console.WriteLine("Нет");
 
             Console.ReadKey();
